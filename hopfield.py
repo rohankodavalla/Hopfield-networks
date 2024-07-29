e23124db94694ca8a3886d1dd5a2b9a61a08bd37
Ron <37825947+rohankodavalla@users.noreply.github.com>
2024-07-29 18:05:45 -0400

    Updated Code

---
+++ hopfield.py
@@ -0,0 +1,163 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.misc as sp
+import matplotlib.image as img
+from skimage.color import rgb2gray
+from matplotlib import pyplot as plt
+from skimage.transform import resize
+from skimage.filters import threshold_mean
+
+
+
+def trainer(vector):
+    vector = vector.flatten()
+    #vector = np.reshape(vector, (len(vector)*len(vector)))
+    coefMat = np.zeros([len(vector),len(vector)])
+    for i in range(len(vector)):
+        for j in range(len(vector)):
+            if (i!=(i-j)):
+                coefMat[i][i-j] = vector[i]*vector[i-j]
+    vector = np.reshape(vector, [int(np.sqrt(len(vector))),int(np.sqrt(len(vector)))])
+    return coefMat
+
+def prediction(curuptedVec,coefMat):
+    curuptedVec = curuptedVec.flatten()
+    predictVec = np.zeros(len(curuptedVec))
+    for i in range(len(curuptedVec)):
+        temp = 0
+        for j in range(len(curuptedVec)):
+             temp += coefMat[i][j] * curuptedVec[j]
+        if (temp>0):
+            predictVec[i] = 1
+        if (temp<0):
+            predictVec[i] = -1
+
+    predictVec = np.reshape(predictVec, [int(np.sqrt(len(predictVec))),int(np.sqrt(len(predictVec)))])
+    return predictVec
+'''
+def imageGenerator(imageVector):
+    cleanImage = np.zeros([len(imageVector),len(imageVector)])
+    for i in range(len(imageVector)):
+        for j in range(len(imageVector)):
+               cleanImage[i][j] = 1
+            else:
+                cleanImage[i][j] = -1
+    noisyImage = cleanImage + np.random.normal(4, 4, [len(image),len(image)])
+
+    for i in range(len(image)):
+        for j in range(len(image)):
+            if (noisyImage[i][j] >= 0):
+                noisyImage[i][j] = 1
+            else:
+                noisyImage[i][j] = -1
+    return cleanImage,noisyImage
+
+
+def reshape(data):
+    dim = int(np.sqrt(len(data)))
+    data = np.reshape(data, (dim, dim))
+    return data
+'''
+
+def plot(data, test, predicted, figsize=(5, 6)):
+    #data = [reshape(d) for d in data]
+    #test = [reshape(d) for d in test]
+    #predicted = [reshape(d) for d in predicted]
+
+    fig, axarr = plt.subplots(len(data), 3, figsize=figsize)
+    for i in range(len(data)):
+        if i==0:
+            axarr[i, 0].set_title('Train data')
+            axarr[i, 1].set_title("Input data")
+            axarr[i, 2].set_title('Output data')
+
+        axarr[i, 0].imshow(data[i])
+        axarr[i, 0].axis('off')
+        axarr[i, 1].imshow(test[i])
+        axarr[i, 1].axis('off')
+        axarr[i, 2].imshow(predicted[i])
+        axarr[i, 2].axis('off')
+
+    plt.tight_layout()
+    plt.savefig("/content/hopfieldNeuralNetwork/output.png")
+    plt.show()
+
+def preprocessing(img, w=64, h=64):
+    # Resize image
+    img = resize(img, (w,h), mode='reflect')
+    # Thresholding
+    thresh = threshold_mean(img)
+    binary = img > thresh
+    shift = 2*(binary*1)-1 # Boolian to int
+    # Reshape
+    flatten = np.reshape(img, (w*h))
+    return shift
+
+def get_corrupted_input(input, corruption_level):
+    corrupted = np.copy(input)
+    inv = np.random.binomial(n=1, p=corruption_level, size=len(input))
+    for i, v in enumerate(input):
+        if inv[i]:
+            corrupted[i] = -1 * v
+    return corrupted
+
+#Import the image
+#image = img.imread('dataset/pgms/bird02.png','w').copy()
+
+zero = rgb2gray(img.imread('/content/drive/MyDrive/ass 3 nn/0.png','w')).copy()
+one = rgb2gray(img.imread('/content/drive/MyDrive/ass 3 nn/1.png','w')).copy()
+two = rgb2gray(img.imread('/content/drive/MyDrive/ass 3 nn/2.png','w')).copy()
+three = rgb2gray(img.imread('/content/drive/MyDrive/ass 3 nn/3.png','w')).copy()
+four = rgb2gray(img.imread('/content/drive/MyDrive/ass 3 nn/4.png','w')).copy()
+six = rgb2gray(img.imread('/content/drive/MyDrive/ass 3 nn/6.png','w')).copy()
+dash = rgb2gray(img.imread('/content/drive/MyDrive/ass 3 nn/-.png','w')).copy()
+nine = rgb2gray(img.imread('/content/drive/MyDrive/ass 3 nn/9.png','w')).copy()
+    
+data = [zero, one, two, three, four, six, dash, nine]
+print("Start to data preprocessing...")
+data = [preprocessing(d) for d in data]
+
+vector_list = []
+noisy_list = []
+#coefMatrix_list = []
+predictedVec_list = []
+plt.clf()
+
+plt.figure(figsize=(15,10))
+i=0
+for image in data :
+  #vector,noisyVec = imageGenerator(image)
+  noisyVec = get_corrupted_input(image, 0.25)
+
+  coefMatrix = trainer(image)
+  predictedVec = prediction(noisyVec,coefMatrix)
+  vector_list.append(image)
+  noisy_list.append(noisyVec)
+  predictedVec_list.append(predictedVec)
+
+#vector,noisyVec = imageGenerator(image)
+#coefMatrix = trainer(vector)
+#predictedVec = prediction(noisyVec,coefMatrix)
+
+#plot(vector_list, noisy_list, predictedVec_list)
+
+
+  plt.subplot(8,3,i+1)
+  plt.imshow(image)
+  if i==0 :
+
+    plt.title('imported picture')
+  #plt.subplot(1,4,2)
+  #plt.imshow(vector);
+  #plt.title('cleaned and croped picture')
+  plt.subplot(8,3,i+2)
+  plt.imshow(noisyVec);
+  if i==0 :
+    plt.title('noisy picture')
+  plt.subplot(8,3,i+3)
+  plt.imshow(predictedVec);
+  if i==0 :
+    plt.title('recalled picture')
+  i=i+3
+plt.savefig('hofield.png')
+plt.show()
